e68916020503b0cd6c00787741e2a2be32365036
Marie Moyles <marie.moyles2@mail.dcu.ie>
2016-06-25 08:18:12 +0100

    teams and matches complete

--- dodgeball_league/console.rb
+++ dodgeball_league/console.rb
@@ -18,17 +18,28 @@ team2 = Team.new( {'name' => "Edinburgh Hitters"}, runner )
 team2.save
 t2 = team2.save()
 
-match1 = Match.new( {'home_team_id' => team1.id, 'away_team_id' => team2.id, 'home_team_score' => 2, 'away_team_score' => 3}, runner)
+team3 = Team.new( {'name' => "Glasgow Bruisers"}, runner )
+team3.save
+t3 = team3.save
+
+team4 = Team.new( {'name' => "Dundee Deckers"}, runner )
+team4.save
+t4 = team4.save
+
+
+
+match1 = Match.new( {'home_team_id' => t1.id, 'away_team_id' => t2.id, 'home_team_score' => 2, 'away_team_score' => 3}, runner)
 match1.save()
 m1 = match1.save()
 
+match2 = Match.new( {'home_team_id' => t3.id, 'away_team_id' => t4.id, 'home_team_score' => 5, 'away_team_score' => 4}, runner)
+match2.save()
+m2 = match2.save()
 
+match3 = Match.new( {'home_team_id' => t2.id, 'away_team_id' => t3.id, 'home_team_score' => 4, 'away_team_score' => 4}, runner)
+match3.save()
+m3 = match3.save()
 
-# album1 = Album.new( { 'name' => 'Definitely Maybe', 'artist_id' => artist.id }, runner )
-# album2 = Album.new( { 'name' => 'Whats the Story', 'artist_id' => artist.id}, runner )
-
-# album1.save()
-# album2.save()
 
 binding.pry
 nil
